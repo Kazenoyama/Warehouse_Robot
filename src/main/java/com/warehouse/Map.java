2a0df777be14aa8dc85edcac3a0676db5e05cd1d
TalbiCome <talbi.come@gmail.com>
2025-01-19 10:36:16 +0100

    mapImplementation

---
+++ src/main/java/com/warehouse/Map.java
@@ -0,0 +1,52 @@
+package com.warehouse;
+
+import java.util.ArrayList;
+
+public class Map {
+
+    private Tile[][] map;
+
+    Map(int lengthAxeX, int lengthAxeY) {
+        map = new Tile[lengthAxeX][lengthAxeY];
+        for (int i = 0; i < lengthAxeX; i++) {
+            for (int j = 0; j < lengthAxeY; j++) {
+                map[i][j] = new Tile(TileEnum.PATH);
+            }
+        }
+    }
+
+    public TileEnum getTyleType(int i, int j) {
+        return map[i][j].getType();
+    }
+
+    public void changeTyleType(int i, int j, TileEnum type) {
+        map[i][j].typeofTile = type;
+    }
+
+    public void changeTyleTipeOfRow(Row row, TileEnum type) {
+        if(!areRowIndexValid(row)){
+            throw new IllegalArgumentException("Row index out of bounds");
+        }
+        Pos start = row.getStart();
+        Pos end = row.getEnd();
+
+        if (start.x == end.x) {
+            for (int i = start.y; i <= end.y; i++) {
+                map[start.x][i].typeofTile = type;
+            }
+        } else {
+            for (int i = start.x; i <= end.x; i++) {
+                map[i][start.y].typeofTile = type;
+            }
+        }
+    }
+
+    private boolean areRowIndexValid(Row row) {
+        return isPositionValid(row.getStart()) && isPositionValid(row.getEnd());
+    }
+
+    private boolean isPositionValid(Pos pos) {
+        return  pos.x >= 0 && pos.x < map.length &&
+                pos.y >= 0 && pos.y < map[0].length;
+    }
+}
