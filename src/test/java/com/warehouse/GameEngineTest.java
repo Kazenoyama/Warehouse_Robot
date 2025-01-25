091e31f1ab11d650c5c6e32ab02c2d5c0c4e17ab
Kazenoyama <147516635+Kazenoyama@users.noreply.github.com>
2025-01-25 18:08:58 +0100

    GameEngineTest: add test for position of shelf

--- src/test/java/com/warehouse/GameEngineTest.java
+++ src/test/java/com/warehouse/GameEngineTest.java
@@ -86,6 +86,17 @@ public class GameEngineTest {
         Integer[][] mapTiles = {{1, 1, 1}, {1, 2, 4}, {1, 1, 1},{1, 3, 2}};
         GameEngine gameEngine = new GameEngine(mapTiles);
         assertEquals(10, gameEngine.getListShelf().size());
+        assertEquals(new Pos(0,0), gameEngine.getListShelf().get(0).getPosition());
+        assertEquals(new Pos(0,1), gameEngine.getListShelf().get(1).getPosition());
+        assertEquals(new Pos(0,2), gameEngine.getListShelf().get(2).getPosition());
+        assertEquals(new Pos(1,0), gameEngine.getListShelf().get(3).getPosition());
+        assertEquals(new Pos(1,2), gameEngine.getListShelf().get(4).getPosition());
+        assertEquals(new Pos(2,0), gameEngine.getListShelf().get(5).getPosition());
+        assertEquals(new Pos(2,1), gameEngine.getListShelf().get(6).getPosition());
+        assertEquals(new Pos(2,2), gameEngine.getListShelf().get(7).getPosition());
+        assertEquals(new Pos(3,0), gameEngine.getListShelf().get(8).getPosition());
+        assertEquals(new Pos(3,1), gameEngine.getListShelf().get(9).getPosition());
+
     }
 
 
