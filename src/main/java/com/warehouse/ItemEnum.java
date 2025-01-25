4f31f1b91e185d0a6e7e8a6e15300c5e58be61e8
Kazenoyama <147516635+Kazenoyama@users.noreply.github.com>
2025-01-25 14:41:17 +0100

    ItemEnum : add weight directly in the Enum to access it in ItemFactory

--- src/main/java/com/warehouse/ItemEnum.java
+++ src/main/java/com/warehouse/ItemEnum.java
@@ -1,15 +1,27 @@
 package com.warehouse;
 
 public enum ItemEnum {
-    FOOD(),
-    DRINK(),
-    ELECTRONICS(),
-    CLOTHES(),
-    TOYS(),
-    TOOLS(),
-    FURNITURE(),
-    COMPUTER(),
-    DENTIFRICE(),
-    PAIN(),
-    OTHER()
+    FOOD(2.0),
+    DRINK(1.5),
+    ELECTRONICS(5.0),
+    CLOTHES(1.0),
+    TOYS(3.0),
+    TOOLS(7.5),
+    FURNITURE(20.0),
+    COMPUTER(10.0),
+    DENTIFRICE(0.5),
+    PAIN(1.2),
+    OTHER(0.0);
+
+    private final double weight;
+
+    // Constructor
+    ItemEnum(double weight) {
+        this.weight = weight;
+    }
+
+    // Getter for weight
+    public double getWeight() {
+        return weight;
+    }
 }
--- src/main/java/com/warehouse/ItemFactory.java
+++ src/main/java/com/warehouse/ItemFactory.java
@@ -15,27 +15,27 @@ public class ItemFactory {
     public static Item createItemEnum(ItemEnum itemEnum, int numberOfItem) {
         switch (itemEnum) {
             case FOOD:
-                return new Item("Food", 1 * numberOfItem, numberOfItem);
+                return new Item("Food", (int) itemEnum.getWeight() * numberOfItem, numberOfItem);
             case DRINK:
-                return new Item("Drink", 1 * numberOfItem, numberOfItem);
+                return new Item("Drink", (int) itemEnum.getWeight() * numberOfItem, numberOfItem);
             case ELECTRONICS:
-                return new Item("Electronics", 1 * numberOfItem, numberOfItem);
+                return new Item("Electronics", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
             case CLOTHES:
-                return new Item("Clothes", 1 * numberOfItem, numberOfItem);
+                return new Item("Clothes", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
             case TOYS:
-                return new Item("Toys", 1 * numberOfItem, numberOfItem);
+                return new Item("Toys", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
             case TOOLS:
-                return new Item("Tools", 1 * numberOfItem, numberOfItem);
+                return new Item("Tools", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
             case FURNITURE:
-                return new Item("Furniture", 1 * numberOfItem, numberOfItem);
+                return new Item("Furniture", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
             case COMPUTER:
-                return new Item("Computer", 1 * numberOfItem, numberOfItem);
+                return new Item("Computer", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
             case DENTIFRICE:
-                return new Item("Dentifrice", 1 * numberOfItem, numberOfItem);
+                return new Item("Dentifrice", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
             case PAIN:
-                return new Item("Pain", 1 * numberOfItem, numberOfItem);
+                return new Item("Pain", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
             case OTHER:
-                return new Item("Other", 1 * numberOfItem, numberOfItem);
+                return new Item("Other", (int) itemEnum.getWeight() * numberOfItem, numberOfItem);
             default:
                 throw new IllegalArgumentException("ItemEnum not found");
         }
