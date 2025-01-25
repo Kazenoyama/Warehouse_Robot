eb3d7b45234ca2745792092423f6f469ba95cde0
Kazenoyama <147516635+Kazenoyama@users.noreply.github.com>
2025-01-25 16:29:45 +0100

    Test_pass

--- src/main/java/com/warehouse/ItemFactory.java
+++ src/main/java/com/warehouse/ItemFactory.java
@@ -11,33 +11,4 @@ public class ItemFactory {
 
         return new Item(itemEnum, weight, volume);
     }
-
-    public static Item createItemEnum(ItemEnum itemEnum, int numberOfItem) {
-        switch (itemEnum) {
-            case FOOD:
-                return new Item("Food", (int) itemEnum.getWeight() * numberOfItem, numberOfItem);
-            case DRINK:
-                return new Item("Drink", (int) itemEnum.getWeight() * numberOfItem, numberOfItem);
-            case ELECTRONICS:
-                return new Item("Electronics", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
-            case CLOTHES:
-                return new Item("Clothes", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
-            case TOYS:
-                return new Item("Toys", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
-            case TOOLS:
-                return new Item("Tools", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
-            case FURNITURE:
-                return new Item("Furniture", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
-            case COMPUTER:
-                return new Item("Computer", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
-            case DENTIFRICE:
-                return new Item("Dentifrice", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
-            case PAIN:
-                return new Item("Pain", (int) itemEnum.getWeight()* numberOfItem, numberOfItem);
-            case OTHER:
-                return new Item("Other", (int) itemEnum.getWeight() * numberOfItem, numberOfItem);
-            default:
-                throw new IllegalArgumentException("ItemEnum not found");
-        }
-    }
 }
